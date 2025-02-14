d6625037629349daadad1b8ff5d1242d1110d92e
Aditya Prakash <115403112+Nickroger797@users.noreply.github.com>
2025-02-14 12:49:51 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,6 @@
+echo "Cloning Repo...."
+git clone https://github.com/Nickroger797/TG-CONVERT-BOT /TG-CONVERT-BOT
+cd /MissPerfectURLBOT
+pip3 install -r requirements.txt
+echo "Starting Bot...."
+python3 bot.py
